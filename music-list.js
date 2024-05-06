3a0185f30977af8f434f63c56e46907e5b98f656
vasu1102 <164175261+vasu1102@users.noreply.github.com>
2024-05-06 12:58:39 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+    {
+        name: "born alone die alone",
+        artist: "jarau",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "selfmade",
+        artist: "sidhu moose walla",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "these days",
+        artist: "sidhu moose wala",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Rao shab",
+        artist: "Rao",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "sidhu slow verb",
+        artist: "sidhu moose wala",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Block",
+        artist: "Dhanda nyoliwala",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "B-town",
+        artist: "sidhu mooose wala",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "fuck em all",
+        artist: "sidhu moose wala",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Gap",
+        artist: "vivek",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Rao Sahab",
+        artist: "rao",
+        img: "music-10",
+        src: "music-10"
+    },
+];
